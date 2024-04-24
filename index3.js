3a179965eca8838f78654875221fd64d0696d512
Sarmila Herlinawati <167835544+Sarmila30@users.noreply.github.com>
2024-04-24 14:02:09 +0700

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,5 @@
+// Toggle class active
+const navbarnav = document.querySelector(".navbar-nav");
+document.queryselector("#hamburger-menu").onclick = () => {
+  navbarnav.classlist.toggle("active");
+};
